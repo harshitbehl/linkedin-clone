0575c1d6b04f8b638b5bd8e7a27686a54b8f6e9c
Harshit Behl <harshitbehl@Harshits-MacBook-Air.local>
2021-08-19 21:06:27 -0400

    Initial Setup

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>This is a header</h1>
+      <div className="header__left">
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Header;
