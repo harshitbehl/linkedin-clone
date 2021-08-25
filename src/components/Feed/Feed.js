873bab909661d9816acee0403ed83652f2e6cbc7
Harshit Behl <harshitbehl@Harshits-MacBook-Air.local>
2021-08-25 15:22:01 -0400

    Implementing Feeds

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { Avatar } from "@material-ui/core";
+import InputOption from "./InputOption";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from "./Post";
+import "./Feed.css";
 
 function Feed() {
-  return <div className="feed"></div>;
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <Avatar
+            className="feed__avatar "
+            src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+          />
+          <form action="">
+            <input type="text" placeholder="Start a post" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#7FC05E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#E7A33E" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#FC9294"
+          />
+        </div>
+      </div>
+
+      <Post name="" description="" message="" photoUrl="" />
+    </div>
+  );
 }
 
 export default Feed;
