a270b194d4340cf3931e58e38ad3ba666b198332
Harshit Behl <harshitbehl@Harshits-MacBook-Air.local>
2021-08-30 22:05:42 -0400

    Redux Implementation

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Avatar } from "@material-ui/core";
 import InputOption from "./InputOption";
 import ImageIcon from "@material-ui/icons/Image";
@@ -7,19 +7,59 @@ import EventNoteIcon from "@material-ui/icons/EventNote";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import Post from "./Post";
 import "./Feed.css";
+import { db } from "../../firebase";
+import firebase from "firebase";
 
 function Feed() {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  // First Render/ Mount Only
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
+  const sendPost = (event) => {
+    event.preventDefault();
+
+    db.collection("posts").add({
+      name: "Harshit Behl",
+      description: "Full Stack Web Developer",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <Avatar
-            className="feed__avatar "
+            className="feed__avatar"
             src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
           />
-          <form action="">
-            <input type="text" placeholder="Start a post" />
-            <button type="submit">Send</button>
+          <form>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+              placeholder="Start a post"
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
 
@@ -35,7 +75,17 @@ function Feed() {
         </div>
       </div>
 
-      <Post name="" description="" message="" photoUrl="" />
+      <hr className="feed__divider" />
+
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
     </div>
   );
 }
--- src/components/Feed/Post.js
+++ src/components/Feed/Post.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import './Post.css';
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import "./Post.css";
+import InputOption from "./InputOption";
+import {
+  ChatOutlined,
+  SendOutlined,
+  ShareOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
 
-function Post({name, description, message, photoUrl}) {
-    return (
-        <div className="post">
-
-            
+function Post({ name, description, message, photoUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar className="post__avatar" />
+        <div className="post__info">
+          <h4>{name}</h4>
+          <p>{description}</p>
         </div>
-    )
+      </div>
+
+      <div className="post__body">
+        <p>{message}</p>
+      </div>
+
+      <hr className="post__divider" />
+
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpAltOutlined} title="Like" />
+        <InputOption Icon={ChatOutlined} title="Comment" />
+        <InputOption Icon={ShareOutlined} title="Share" />
+        <InputOption Icon={SendOutlined} title="Send" />
+      </div>
+    </div>
+  );
 }
 
 export default Post;
